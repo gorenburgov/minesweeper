cb53ec6cd5bf3b7d66ff52259c1b404972beb3b9
Yury Gorenburgov <gorenburgov@gmail.com>
2020-05-07 10:50:42 +0300

    build for github pages

---
+++ precache-manifest.9f9ea30317a9072cf60d7fb070edb4be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf38c6bbb6e22f3af3789040f6222748",
+    "url": "/index.html"
+  },
+  {
+    "revision": "063fda5289e5e8f54951",
+    "url": "/static/css/main.28a70157.chunk.css"
+  },
+  {
+    "revision": "3984aa2efe328ecb9b6a",
+    "url": "/static/js/2.6ec516b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6ec516b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "063fda5289e5e8f54951",
+    "url": "/static/js/main.f2cea9dc.chunk.js"
+  },
+  {
+    "revision": "e8948ffb8948f0d9fe9d",
+    "url": "/static/js/runtime-main.1c96a9e1.js"
+  }
+]);
--- src/components/game/IndicationPanel.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default ({ flags, message, color }) => {
-    return (
-        <div className="indication-panel">
-            <div>Flags remained: {flags}</div>
-            <div className={color}>{message}</div>
-        </div>
-    );
-};
